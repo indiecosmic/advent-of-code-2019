05630cb3f680b3e31a2bf2141dcad217ed2838ef
Mattias Rudén <mattias@indiesoft.org>
2019-12-05 21:42:01 +0100

    Part 1

--- solutions/day5.py
+++ solutions/day5.py
@@ -2,24 +2,59 @@ from typing import List
 from math import floor
 
 def opcode(value:int):
-    return value % 10
+    return int(str(value)[-2:])
 
 def mode(value:int):
     return (floor(value//100) % 10, floor(value//1000) % 10, floor(value//10000) % 10)
 
-def run_opcode(index:int, instructions: List[int]):
+def run_opcode(index:int, instructions: List[int], input: int = 1):
     op = opcode(instructions[index])
+    m = mode(instructions[index])
     if (op == 99):
-        return
-    pos1 = instructions[index+1]
-    pos2 = instructions[index+2]
-    target = instructions[index+3]
-    if (op == 1):           
-        instructions[target] = instructions[pos1] + instructions[pos2]
+        return instructions
+    if (op == 1):
+        pos1 = instructions[index+1]
+        pos2 = instructions[index+2]
+        target = instructions[index+3]
+        val1 = instructions[pos1] if m[0] == 0 else pos1
+        val2 = instructions[pos2] if m[1] == 0 else pos2
+        instructions[target] = val1 + val2
+        return instructions
     elif (op == 2):
-        instructions[target] = instructions[pos1] * instructions[pos2]
+        pos1 = instructions[index+1]
+        pos2 = instructions[index+2]
+        target = instructions[index+3]
+        val1 = instructions[pos1] if m[0] == 0 else pos1
+        val2 = instructions[pos2] if m[1] == 0 else pos2
+        instructions[target] = val1 * val2
+        return instructions
     elif (op == 3):
-        parameter = instructions[index+1]
-        
+        target = instructions[index+1]
+        instructions[target] = input
+        return instructions
+    elif op == 4:
+        target = instructions[index+1]
+        print(instructions[target])
+        return instructions
     else:
-        raise Exception('invalid opcode: {}'.format(opcode))
+        raise Exception('invalid opcode: {}'.format(op))
+    return instructions
+
+def part1(program):
+    index = 0
+    while index < len(program):
+        op = opcode(program[index])
+        if op == 99:
+            break
+        program = run_opcode(index, program)
+        if op == 1 or op == 2:
+            index += 4
+        else:
+            index += 2
+
+if __name__ == '__main__':
+    with open('day5.txt') as f:
+        contents = f.read()
+        program = contents.strip().split(',')
+        program = list(map(int, program))
+        part1(program)
--- tests/test_day5.py
+++ tests/test_day5.py
@@ -1,11 +1,20 @@
 import pytest
 from solutions.day5 import opcode
 from solutions.day5 import mode
+from solutions.day5 import run_opcode
 
-@pytest.mark.parametrize("test_input,expected", [(1002, 2), (1101, 1)])
+@pytest.mark.parametrize("test_input,expected", [(1002, 2), (1101, 1), (99, 99)])
 def test_opcode(test_input, expected):
     assert opcode(test_input) == expected
 
 @pytest.mark.parametrize("test_input,expected", [(1002, (0,1,0))])
 def test_mode(test_input, expected):
     assert mode(test_input) == expected
+
+@pytest.mark.parametrize("index, instructions, expected", [
+    (0, [1002,4,3,4,33], [1002,4,3,4,99]),
+    (0, [1,0,0,0,99], [2,0,0,0,99]),
+    (0, [1101,100,-1,4,0], [1101,100,-1,4,99])
+    ])
+def test_run_opcode(index, instructions, expected):
+    assert run_opcode(index, instructions) == expected
