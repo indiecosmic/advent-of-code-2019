5a20cc50d992d4b1a8f4ef7719f323905b884a21
Mattias Rudén <mattias@indiesoft.org>
2019-12-06 21:24:40 +0100

    Part 2

--- solutions/day6.py
+++ solutions/day6.py
@@ -51,16 +51,6 @@ def total_orbit_count(map: List[str]):
     
     return count
 
-def create_node_list(map):
-    planets = []
-    for obj in map:
-        parent, child = obj.split(')')
-        if parent not in planets:
-            planets.append(parent)
-        if child not in planets:
-            planets.append(child)
-    return planets
-
 def create_tree(objects) -> Tree:
     map = create_map(objects)
     root = Tree('COM')
@@ -86,8 +76,45 @@ def find_in_tree(key, tree):
             return res
     return None
 
+def get_parent_list(node):
+    parents = []
+    while node.parent != None:
+        node = node.parent
+        parents.append(node.name)
+    return parents
+
+def find_common_parent(key1, key2, tree):
+    node1 = find_in_tree(key1, tree)
+    node2 = find_in_tree(key2, tree)
+    parents1 = get_parent_list(node1)
+    parents2 = get_parent_list(node2)
+    for p in parents1:
+        if p in parents2:
+            return p
+
+def number_of_transfers(from_key, to_key, tree):
+    common_key = find_common_parent(from_key, to_key, tree)
+    from_node = find_in_tree(from_key, tree)
+    to_node = find_in_tree(to_key, tree)
+    return count_steps(from_node.parent, common_key) + count_steps(to_node.parent, common_key)
+
+def count_steps(node, key):
+    count = 0
+    while node.name != key:
+        node = node.parent
+        count += 1    
+    return count
+
+def part1(map):
+    return total_orbit_count(map)
+
+def part2(map):
+    tree = create_tree(map)
+    return number_of_transfers('YOU', 'SAN', tree)
+
 if __name__ == '__main__':
     with open('day6.txt') as f:
         contents = f.read()
         planets = contents.strip().split('\n')
-        print(total_orbit_count(planets))
+        print(part1(planets))
+        print(part2(planets))
--- tests/test_day6.py
+++ tests/test_day6.py
@@ -1,5 +1,5 @@
 import pytest
-from solutions.day6 import create_map,orbit_count, create_tree, total_orbit_count
+from solutions.day6 import create_map,orbit_count, create_tree, total_orbit_count, find_common_parent, number_of_transfers
 
 @pytest.mark.parametrize("test_input,expected", [
     (["COM)B"], {'COM': ['B']}),
@@ -14,3 +14,13 @@ def test_orbit_count():
 
 def test_total_orbit_count():
     assert total_orbit_count(['COM)B', 'B)C', 'C)D','D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L']) == 42
+
+def test_find_common_parent():
+    tree = create_tree(['COM)B','B)C','C)D','D)E','E)F','B)G','G)H','D)I','E)J','J)K','K)L','K)YOU','I)SAN'])
+    common = find_common_parent('YOU', 'SAN', tree)
+    assert common == 'D'
+
+def test_number_of_transfers():
+    tree = create_tree(['COM)B','B)C','C)D','D)E','E)F','B)G','G)H','D)I','E)J','J)K','K)L','K)YOU','I)SAN'])
+    count = number_of_transfers('YOU', 'SAN', tree)
+    assert count == 4
