5163a4375fe5df9ecd48664e3b4d226ec187b255
Mattias Rudén <mattias@indiesoft.org>
2019-12-08 12:17:10 +0100

    Part 2

--- solutions/day8.py
+++ solutions/day8.py
@@ -18,6 +18,19 @@ def create_layers(numbers:str, width:int, height:int):
 def count_occurences(value:str, layer:List[str]):
     return sum([row.count(value) for row in layer])
 
+def merge_layers(layers, width, height):
+    res = []
+    for rownum in range(height):
+        row = ''
+        for colnum in range(width):
+            for layernum in range(len(layers)):
+                current = layers[layernum][rownum][colnum]
+                if current in ['0','1']:
+                    row += current
+                    break
+        res.append(row)
+    return res
+
 def part1(layers):
     min = maxsize
     res = []
@@ -28,8 +41,16 @@ def part1(layers):
             res = layer
     return count_occurences('1', res) * count_occurences('2', res)
 
+def part2(layers, width, height):
+    merged = merge_layers(layers, width, height)
+    res = ''
+    for row in merged:
+        res += row.replace('0', ' ') + '\n'
+    return res
+
 if __name__ == "__main__":
     with open('day8.txt') as f:
         contents = f.read()
         layers = create_layers(contents, 25, 6)
         print(part1(layers))
+        print(part2(layers, 25, 6))
--- tests/test_day8.py
+++ tests/test_day8.py
@@ -1,6 +1,7 @@
 import pytest
 from solutions.day8 import create_layers
 from solutions.day8 import count_occurences
+from solutions.day8 import merge_layers
 
 @pytest.mark.parametrize("input, expected", [
     ('123456789012', [['123','456'], ['789','012']])
@@ -13,3 +14,10 @@ def test_create_layers(input, expected):
 ])
 def test_count_occurences(value, layer, expected):
     assert count_occurences(value, layer) == expected
+
+@pytest.mark.parametrize('input, expected', [
+    ('0222112222120000', ['01', '10'])
+])
+def test_merge_layers(input, expected):
+    layers = create_layers(input, 2, 2)
+    assert merge_layers(layers, 2, 2) == expected
