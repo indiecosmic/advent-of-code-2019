cdcf8d6ea4dbe7d8f00b7cc9268a8d6a5be7f804
Mattias Rudén <mattias@indiesoft.org>
2019-12-10 14:59:40 +0100

    Part 1

--- solutions/day10.py
+++ solutions/day10.py
@@ -1,4 +1,6 @@
 from typing import List
+from shapely.geometry import Point
+from shapely.geometry import LineString
 
 def parse_map(input: str):
     return input.split('\n')
@@ -11,8 +13,35 @@ def find_coordinates(map: List[str]):
                 coordinates.append((x,y))
     return coordinates
 
-def find_visible(position, coordinates):
-    return []
-
 def can_detect(position, target, asteroids):
-    return False
+    obstacles = set(asteroids) - set([position,target])
+    line = LineString([position, target])
+    for o in obstacles:
+        if line.intersects(Point(o)):
+            return False
+    return True
+
+def find_best_location(asteroids):
+    max = 0
+    maxpos = ()
+    for pos in asteroids:
+        count = 0
+        for target in asteroids:
+            if pos == target:
+                continue
+            if can_detect(pos, target, asteroids):
+                count += 1
+        if count > max:
+            max = count
+            maxpos = pos
+    return (maxpos, max)
+
+def part1():
+    pass
+
+if __name__ == "__main__":
+    with open('day10.txt') as f:
+        contents = f.read()
+        map = parse_map(contents)
+        asteroids = find_coordinates(map)
+        print(find_best_location(asteroids))
--- tests/test_day10.py
+++ tests/test_day10.py
@@ -1,7 +1,8 @@
+import pytest
 from solutions.day10 import parse_map
 from solutions.day10 import find_coordinates
-from solutions.day10 import find_visible
 from solutions.day10 import can_detect
+from solutions.day10 import find_best_location
 
 def test_parse_map():
     input = str('.#..#\n'+
@@ -17,5 +18,17 @@ def test_find_coordinates():
     result = find_coordinates(input)
     assert result == [(1,0),(4,0),(0,2),(1,2),(2,2),(3,2),(4,2),(4,3),(3,4),(4,4)]
 
-def test_can_detect(position, target, asteroids):
-    pass
+@pytest.mark.parametrize("position,target,asteroids,expected", [
+    ((4,2), (3,2), [(1,0),(4,0),(0,2),(1,2),(2,2),(3,2),(4,2),(4,3),(3,4),(4,4)], True),
+    ((4,2), (2,2), [(1,0),(4,0),(0,2),(1,2),(2,2),(3,2),(4,2),(4,3),(3,4),(4,4)], False),
+    ((4,2), (4,4), [(1,0),(4,0),(0,2),(1,2),(2,2),(3,2),(4,2),(4,3),(3,4),(4,4)], False),
+    ((4,2), (1,0), [(1,0),(4,0),(0,2),(1,2),(2,2),(3,2),(4,2),(4,3),(3,4),(4,4)], True)
+])
+def test_can_detect(position, target, asteroids, expected):
+    assert can_detect(position, target, asteroids) == expected
+
+@pytest.mark.parametrize("asteroids,expected", [
+    ([(1,0),(4,0),(0,2),(1,2),(2,2),(3,2),(4,2),(4,3),(3,4),(4,4)], ((3,4),8)),
+])
+def test_find_best_location(asteroids, expected):
+    assert find_best_location(asteroids) == expected
