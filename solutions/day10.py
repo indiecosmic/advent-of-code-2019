541ab6216ca8518ef066e574af21b476f0c55bed
Mattias Rudén <mattias@indiesoft.org>
2019-12-10 18:59:34 +0100

    Optimization

--- solutions/day10.py
+++ solutions/day10.py
@@ -1,6 +1,6 @@
 from typing import List
-from shapely.geometry import Point
-from shapely.geometry import LineString
+from math import sqrt
+from math import isclose
 
 def parse_map(input: str):
     return input.split('\n')
@@ -15,11 +15,20 @@ def find_coordinates(map: List[str]):
 
 def can_detect(position, target, asteroids):
     obstacles = set(asteroids) - set([position,target])
-    line = LineString([position, target])
     for o in obstacles:
-        if line.intersects(Point(o)):
+        if o[0] < min(position[0], target[0]) or o[0] > max(position[0], target[0]):
+            continue
+        if o[1] < min(position[1], target[1]) or o[1] > max(position[1], target[1]):
+            continue
+        if is_between(position, o, target):
             return False
     return True
+
+def distance(a,b):
+    return sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+def is_between(a,c,b):
+    return isclose(distance(a,c) + distance(c,b), distance(a,b))
 
 def find_best_location(asteroids):
     max = 0
@@ -36,12 +45,12 @@ def find_best_location(asteroids):
             maxpos = pos
     return (maxpos, max)
 
-def part1():
-    pass
+def part1(asteroids):
+    return find_best_location(asteroids)
 
 if __name__ == "__main__":
     with open('day10.txt') as f:
         contents = f.read()
         map = parse_map(contents)
         asteroids = find_coordinates(map)
-        print(find_best_location(asteroids))
+        print(part1(asteroids))
