3e72b08e08a79cd8ffe60b5e75f87a793f2e888b
Mattias Rudén <mattias@indiesoft.org>
2019-12-11 15:08:01 +0100

    Replace intcode memory with dictionary

--- common/Intcode.py
+++ common/Intcode.py
@@ -1,10 +1,11 @@
 from math import floor
 from typing import List
+from collections import defaultdict
 
 class Intcode:
     def __init__(self, instructions: List[int], inputs: List[int], allow_grow: bool = False):
-        self.instructions = instructions.copy()
-        self.current_instruction = 0
+        self.memory = defaultdict(lambda: 0, enumerate(instructions))
+        self.ptr = 0
         self.inputs = inputs
         self.outputs = []
         self.current_input = 0
@@ -14,36 +15,28 @@ class Intcode:
         self.relative_base = 0
         self.allow_grow = allow_grow
 
-    def get_opcode(self, value:int):
-        return int(str(value)[-2:])
+    def get_opcode(self):
+        return self.memory[self.ptr] % 100
 
-    def get_mode(self, value:int):
+    def get_mode(self):
+        value = self.memory[self.ptr]
         return (floor(value//100) % 10, floor(value//1000) % 10, floor(value//10000) % 10)
 
-    def extend(self, index, instructions):
-        if not self.allow_grow:
-            return
-        if (index >= len(instructions)):
-                instructions.extend([0]*(index + 1 - len(instructions)))
-
-    def get_values(self, index, modes, instructions: List[int]):
-        val1 = self.get_value(index + 1, modes[0], instructions)
-        val2 = self.get_value(index + 2, modes[1], instructions)
-        target = self.get_value(index + 3, 1, instructions)
+    def get_values(self, modes):
+        val1 = self.get_value(self.ptr + 1, modes[0])
+        val2 = self.get_value(self.ptr + 2, modes[1])
+        target = self.get_value(self.ptr + 3, 1)
         return (val1, val2, target)
 
-    def get_value(self, index, mode, instructions):
+    def get_value(self, index, mode):
         if mode == 0:
-            pos = instructions[index]
-            self.extend(pos, instructions)
-            return instructions[pos]
+            pos = self.memory[index]
+            return self.memory[pos]
         elif mode == 1:
-            self.extend(index, instructions)
-            return instructions[index]
+            return self.memory[index]
         elif mode == 2:
-            pos = instructions[index] + self.relative_base
-            self.extend(pos, instructions)
-            return instructions[pos]
+            pos = self.memory[index] + self.relative_base
+            return self.memory[pos]
         else:
             raise Exception('invalid mode: {}'.format(mode))
 
@@ -73,67 +66,61 @@ class Intcode:
         else:
             self.inputs.append(value)
 
-    def set_value(self, index, mode, instructions, value):
+    def set_value(self, index, mode, value):
         if mode == 0:
-            pos = instructions[index]
-            self.extend(pos, instructions)
-            instructions[pos] = value
+            pos = self.memory[index]
+            self.memory[pos] = value
         elif mode == 2:
-            pos = instructions[index] + self.relative_base
-            self.extend(pos, instructions)
-            instructions[pos] = value
+            pos = self.memory[index] + self.relative_base
+            self.memory[pos] = value
         else:
             raise Exception('invalid mode: {}'.format(mode))
 
-    def run_opcode(self, index:int, instructions: List[int]):
-        op = self.get_opcode(instructions[index])
-        m = self.get_mode(instructions[index])
+    def run_opcode(self):
+        op = self.get_opcode()
+        mode = self.get_mode()
         if (op == 99):
             return None
         if (op == 1):
-            values = self.get_values(index, m, instructions)
-            self.set_value(index + 3, m[2], instructions, values[0] + values[1])
-            return index + 4
+            values = self.get_values(mode)
+            self.set_value(self.ptr + 3, mode[2], values[0] + values[1])
+            return self.ptr + 4
         elif (op == 2):
-            values = self.get_values(index, m, instructions)
-            self.extend(values[0], instructions)
-            self.extend(values[1], instructions)
-            self.extend(values[2], instructions)
-            self.set_value(index + 3, m[2], instructions, values[0] * values[1])
-            return index + 4
+            values = self.get_values(mode)
+            self.set_value(self.ptr + 3, mode[2], values[0] * values[1])
+            return self.ptr + 4
         elif (op == 3):
             input = self.get_input()
-            self.set_value(index + 1, m[0], instructions, input)
-            return index + 2
+            self.set_value(self.ptr + 1, mode[0], input)
+            return self.ptr + 2
         elif op == 4:
-            value = self.get_value(index + 1, m[0], instructions)
+            value = self.get_value(self.ptr + 1, mode[0])
             self.set_output(value)
-            return index + 2
+            return self.ptr + 2
         elif op == 5:
-            param1 = self.get_value(index + 1, m[0], instructions)
+            param1 = self.get_value(self.ptr + 1, mode[0])
             if param1 != 0:
-                return self.get_value(index + 2, m[1], instructions)
-            return index + 3
+                return self.get_value(self.ptr + 2, mode[1])
+            return self.ptr + 3
         elif op == 6:
-            param1 = self.get_value(index + 1, m[0], instructions)
+            param1 = self.get_value(self.ptr + 1, mode[0])
             if param1 == 0:
-                return self.get_value(index + 2, m[1], instructions)
-            return index + 3
+                return self.get_value(self.ptr + 2, mode[1])
+            return self.ptr + 3
         elif op == 7:
-            values = self.get_values(index, m, instructions)
+            values = self.get_values(mode)
             value = 1 if values[0] < values[1] else 0
-            self.set_value(index + 3, m[2], instructions, value)
-            return index + 4
+            self.set_value(self.ptr + 3, mode[2], value)
+            return self.ptr + 4
         elif op == 8:
-            values = self.get_values(index, m, instructions)
-            self.extend(values[2], instructions)
+            values = self.get_values(mode)
             value = 1 if values[0] == values[1] else 0
-            self.set_value(index + 3, m[2], instructions, value)
-            return index + 4
+            self.set_value(self.ptr + 3, mode[2], value)
+            return self.ptr + 4
         elif op == 9:
-            param1 = self.get_value(index + 1, m[0], instructions)
+            param1 = self.get_value(self.ptr + 1, mode[0])
             self.relative_base += param1
-            return index + 2
+            return self.ptr + 2
         else:
             raise Exception('invalid opcode: {}'.format(op))
 
@@ -141,12 +128,12 @@ class Intcode:
         self.outputs = []
         self.current_output = 0
         self.halted = False
-        while self.current_instruction < len(self.instructions):
-            op = self.get_opcode(self.instructions[self.current_instruction])
+        while True :
+            op = self.get_opcode()
             if op == 99:
                 self.stopped = True
                 break
             if op == 3 and self.should_wait_for_input():
                 self.halted = True
                 break
-            self.current_instruction = self.run_opcode(self.current_instruction, self.instructions)
+            self.ptr = self.run_opcode()
