10e8565e56a0f3a84d2147e24ccfdd3b5e15d889
Mattias Rudén <mattias@indiesoft.org>
2019-12-16 15:55:08 +0100

    Dunno

---
+++ solutions/day16.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def create_input_signal(input: str):
+    return list(map(int, [s for s in input]))
+
+def calculate_signal(input_signal: List[int], base_pattern: List[int]):
+    result = []
+    base_offset = 0
+    for i in range(len(input_signal)):
+        
+
+def part1():
+    pass
