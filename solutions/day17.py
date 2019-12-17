97cddb1988c6e0fc6a34b10bd121b6445809747f
Mattias Rudén <mattias@indiesoft.org>
2019-12-17 21:40:17 +0100

    Part 2

--- solutions/day17.py
+++ solutions/day17.py
@@ -1,13 +1,20 @@
 import os
+from typing import List
+from typing import Dict
+from typing import Tuple
 from common.Intcode import Intcode
 from common.Point import Point
+
+class Robot:
+    def __init__(self, pos: Point, dir: Point):
+        self.pos = pos
+        self.dir = dir
 
 def render(map):
     x_min = min([key.x for key in map])
     x_max = max([key.x for key in map])
     y_min = min([key.y for key in map])
     y_max = max([key.y for key in map])
-    print(x_min, x_max, y_min, y_max)
     for y in range(y_min, y_max + 1):
         row = ''
         for x in range(x_min, x_max + 1):
@@ -16,6 +23,9 @@ def render(map):
             else:
                 row += map[(x,y)]
         print(row)
+
+def print_message(ascii: List[int]):
+    print([chr(c) for c in ascii])
 
 def is_intersection(pos: Point, map):
     neighbours = get_connected_tiles(pos, map)
@@ -38,29 +48,111 @@ def get_connected_tiles(pos: Point, map):
             connected.append(c)
     return connected
 
+def walk_to_edge(robot, dir, map):
+    steps = 0
+    pos = robot
+    while True:
+        pos = Point(pos.x + dir.x, pos.y + dir.y)
+        if pos in map and map[pos] != '.':
+            steps += 1
+        else:
+            break
+    return steps
+
+def turn(robot, dir, map):
+    # can turn right
+    turn = Point(-dir.y, dir.x)
+    next = Point(robot.x + turn.x, robot.y + turn.y)
+    if next in map and map[next] != '.':
+        return ('R', turn)
+    # can turn right
+    turn = Point(dir.y, -dir.x)
+    next = Point(robot.x + turn.x, robot.y + turn.y)
+    if next in map and map[next] != '.':
+        return ('L', turn)
+    return None
+
+def convert_to_ascii(text: str):
+    res = []
+    for t in text:
+        res.append(ord(t))
+    return res + [10]
+
 def part1(instructions):
     computer = Intcode(instructions, [])
     computer.run_program()
-    map = {}
-    y = 0
-    x = 0
-    for o in computer.outputs:
-        if o == 10:
-            y += 1
-            x = 0
-        else:
-            map[Point(x, y)] = chr(o)
-            x += 1
+    map = create_map(computer.outputs)
     render(map)
-    scaffolds = [key  for (key, value) in map.items() if value in ['#','^','v','<','>']]
-    intersections = []
-    for s in scaffolds:
-        if is_intersection(s, map):
-            intersections.append(s)
+    intersections = get_intersections(map)
     parameters = []
     for i in intersections:
         parameters.append(i.x * i.y)
     print(sum(parameters))
+
+def part2(instructions):
+    computer = Intcode(instructions, [])
+    computer.run_program()
+    map = create_map(computer.outputs)
+    robot = [key for (key,value) in map.items() if value == '^'][0]
+    dir = Point(0, -1)
+    movements = []
+    while True:
+        #walk to edge
+        dist = walk_to_edge(robot, dir, map)
+        if dist > 0:
+            movements.append(dist)
+            robot = Point(robot.x + dir.x * dist, robot.y + dir.y * dist)
+        #try turn
+        direction = turn(robot, dir, map)
+        if direction != None:
+            movements.append(direction[0])
+            dir = direction[1]
+        if dist == 0 and direction == None:
+            break
+    print(movements)
+    computer = Intcode(instructions, [])
+    computer.memory[0] = 2
+    computer.run_program()
+    #Main routine: [A,B,B,C,C,A,B,B,C,A]
+    computer.inputs += convert_to_ascii('A,B,B,C,C,A,B,B,C,A')
+    computer.run_program()
+    print_message(computer.outputs)
+    #Function A: 'R', 4, 'R', 12, 'R', 10, 'L', 12
+    computer.inputs += convert_to_ascii('R,4,R,12,R,10,L,12')
+    computer.run_program()
+    print_message(computer.outputs)
+    #Function B: 'L', 12, 'R', 4, 'R', 12
+    computer.inputs += convert_to_ascii('L,12,R,4,R,12')
+    computer.run_program()
+    print_message(computer.outputs)
+    #Function C: 'L', 12, 'L', 8, 'R', 10
+    computer.inputs += convert_to_ascii('L,12,L,8,R,10')
+    computer.run_program()
+    print_message(computer.outputs)
+    computer.inputs += convert_to_ascii('n')
+    computer.run_program()
+    print(computer.outputs[-1])
+
+def create_map(inputs: List[int]) -> Dict[Point, str]:
+    map = {}
+    y = 0
+    x = 0
+    for i in inputs:
+        if i == 10:
+            y += 1
+            x = 0
+        else:
+            map[Point(x, y)] = chr(i)
+            x += 1
+    return map
+
+def get_intersections(map):
+    intersections = []
+    scaffolds = [key  for (key, value) in map.items() if value in ['#','^','v','<','>']]
+    for s in scaffolds:
+        if is_intersection(s, map):
+            intersections.append(s)
+    return intersections
 
 def get_input():
     file_path = os.path.join(os.path.dirname(__file__), 'day17.txt')
@@ -73,6 +165,7 @@ def get_input():
 def main():
     instructions = get_input()
     part1(instructions)
+    part2(instructions)
 
 if __name__ == "__main__":
     main()
