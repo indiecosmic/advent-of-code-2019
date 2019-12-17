d6a9fab3d6829d9c432d80e0d8b5d998410c3686
Mattias Rudén <mattias@indiesoft.org>
2019-12-17 23:12:11 +0100

    Temp

--- solutions/day14.py
+++ solutions/day14.py
@@ -1,26 +1,54 @@
+import os
 from collections import namedtuple
 from typing import List
 from math import ceil
 
+
 Chemical = namedtuple('Chemical', 'output, components')
 Component = namedtuple('Component', 'count, chemical')
 
-def get_reactions(chemical, amount, cookbook, reaction_list, waste):
+def get_needed_ingredients(chemical, amount, cookbook, required):
+    if chemical not in cookbook:
+        return
+    if chemical not in required:
+        required[chemical] = amount
+    else:
+        required[chemical] += amount
     recipe = cookbook[chemical]
-    multiplier = ceil(amount/recipe[0])
-    if chemical not in waste:
-        waste[chemical] = 0
-    waste[chemical] += recipe[0] % amount
-    reactions = []
-    for reaction in recipe[1]:
-        if waste[chemical] >= reaction[1] * multiplier:
-            waste[chemical] -= reaction[1] * multiplier
+    multiplier = ceil(amount/recipe.output)
+    for component in recipe.components:
+        get_needed_ingredients(component.chemical, component.count * multiplier, cookbook, required)
+
+def get_needed_ore(required, cookbook):
+    sum = 0
+    for c, a in required.items():
+        recipe = cookbook[c]
+        for comp in recipe.components:
+            if comp.chemical == 'ORE':
+                sum += (ceil(a/recipe.output) * comp.count)
+    return sum
+
+def get_reactions(chemical, amount, cookbook, reaction_list, waste):
+    if chemical in waste:
+        if waste[chemical] >= amount:
+            waste[chemical] -= amount
+            return
         else:
-            reactions.append((reaction[0], reaction[1] * multiplier))
-    for r, a in reactions:
-        if r != 'ORE':
-            get_reactions(r, a, cookbook, reaction_list, waste)
-    reaction_list += reactions
+            amount -= waste[chemical]
+            waste[chemical] = 0
+    else:
+        waste[chemical] = 0
+
+    if chemical not in cookbook:
+        waste[chemical] += amount
+        reaction_list.append(amount)
+        return
+    recipe = cookbook[chemical]
+    multiplier = ceil(amount/recipe.output)
+
+    waste[chemical] += recipe.output % amount
+    for component in recipe.components:
+        get_reactions(component.chemical, component.count * multiplier, cookbook, reaction_list, waste)
 
 def create_cookbook(input: str):
     lines = input.splitlines()
@@ -29,7 +57,26 @@ def create_cookbook(input: str):
         input, output = line.split('=>')
         components = [i for i in input.split(',')]
         components = [c.strip().split(' ') for c in components]
-        components = [(c[1], int(c[0])) for c in components]
+        components = [Component(int(c[0]),c[1]) for c in components]
         amount, chemical = output.strip().split(' ')
-        result[chemical] = (int(amount), components)
+        result[chemical] = Chemical(int(amount), components)
     return result
+
+def get_input():
+    file_path = os.path.join(os.path.dirname(__file__), 'day14.txt')
+    with open(file_path) as f:
+        contents = f.read()
+        return contents
+
+def part1(input: str):
+    cookbook = create_cookbook(input)
+    required = {}
+    get_needed_ingredients('FUEL', 1, cookbook, required)
+    return get_needed_ore(required, cookbook)
+
+def main():
+    input = get_input()
+    print(part1(input))
+
+if __name__ == "__main__":
+    main()
--- tests/test_day14.py
+++ tests/test_day14.py
@@ -1,6 +1,8 @@
 import pytest
 from solutions.day14 import create_cookbook
 from solutions.day14 import get_reactions
+from solutions.day14 import get_needed_ingredients
+from solutions.day14 import get_needed_ore
 
 @pytest.mark.parametrize("chemical,amount,input,expected", [
     ('A', 10, str(
@@ -31,8 +33,61 @@ def test_get_reactions(chemical, amount, input, expected):
     result = []
     waste = {}
     get_reactions(chemical, amount, cookbook, result, waste)
-    ore_required = sum([a for r,a in result if r == 'ORE'])
+    ore_required = waste['ORE']
     assert ore_required == expected
+
+@pytest.mark.parametrize("chemical,amount,input,expected", [
+    ('A', 10, str(
+        '10 ORE => 10 A\n'
+        '1 ORE => 1 B\n'
+        '7 A, 1 B => 1 C\n'
+        '7 A, 1 C => 1 D\n'
+        '7 A, 1 D => 1 E\n'
+        '7 A, 1 E => 1 FUEL\n'), 10),
+    ('FUEL', 1, str(
+        '10 ORE => 10 A\n'
+        '1 ORE => 1 B\n'
+        '7 A, 1 B => 1 C\n'
+        '7 A, 1 C => 1 D\n'
+        '7 A, 1 D => 1 E\n'
+        '7 A, 1 E => 1 FUEL\n'), 31),
+    ('FUEL', 1, str(
+        '9 ORE => 2 A\n'
+        '8 ORE => 3 B\n'
+        '7 ORE => 5 C\n'
+        '3 A, 4 B => 1 AB\n'
+        '5 B, 7 C => 1 BC\n'
+        '4 C, 1 A => 1 CA\n'
+        '2 AB, 3 BC, 4 CA => 1 FUEL\n'), 165),
+    ('FUEL', 1, str(
+        '157 ORE => 5 NZVS\n'
+        '165 ORE => 6 DCFZ\n'
+        '44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL\n'
+        '12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ\n'
+        '179 ORE => 7 PSHF\n'
+        '177 ORE => 5 HKGWZ\n'
+        '7 DCFZ, 7 PSHF => 2 XJWVT\n'
+        '165 ORE => 2 GPVTF\n'
+        '3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT\n'), 13312),
+    ('FUEL', 1, str(
+        '2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG\n'
+        '17 NVRVD, 3 JNWZP => 8 VPVL\n'
+        '53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL\n'
+        '22 VJHF, 37 MNCFX => 5 FWMGM\n'
+        '139 ORE => 4 NVRVD\n'
+        '144 ORE => 7 JNWZP\n'
+        '5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC\n'
+        '5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV\n'
+        '145 ORE => 6 MNCFX\n'
+        '1 NVRVD => 8 CXFTF\n'
+        '1 VJHF, 6 MNCFX => 4 RFSQX\n'
+        '176 ORE => 6 VJHF'), 180697)
+    ])
+def test_get_needed_ingredients(chemical, amount, input, expected):
+    cookbook = create_cookbook(input)
+    required = {}
+    get_needed_ingredients(chemical, amount, cookbook, required)
+    assert get_needed_ore(required, cookbook) == expected
 
 def test_create_cookbook():
     input = str(
