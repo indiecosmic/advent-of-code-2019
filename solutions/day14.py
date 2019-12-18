e301960d60f6422a24293ed858dac9c1e46559f2
Mattias Rudén <mattias@indiesoft.org>
2019-12-18 06:37:33 +0100

    Part 2

--- solutions/day14.py
+++ solutions/day14.py
@@ -7,28 +7,8 @@ from math import ceil
 Chemical = namedtuple('Chemical', 'output, components')
 Component = namedtuple('Component', 'count, chemical')
 
-def get_needed_ingredients(chemical, amount, cookbook, required):
-    if chemical not in cookbook:
-        return
-    if chemical not in required:
-        required[chemical] = amount
-    else:
-        required[chemical] += amount
-    recipe = cookbook[chemical]
-    multiplier = ceil(amount/recipe.output)
-    for component in recipe.components:
-        get_needed_ingredients(component.chemical, component.count * multiplier, cookbook, required)
 
-def get_needed_ore(required, cookbook):
-    sum = 0
-    for c, a in required.items():
-        recipe = cookbook[c]
-        for comp in recipe.components:
-            if comp.chemical == 'ORE':
-                sum += (ceil(a/recipe.output) * comp.count)
-    return sum
-
-def get_reactions(chemical, amount, cookbook, store = {}):
+def get_reactions(chemical, amount, cookbook, store={}):
     if chemical in store:
         if store[chemical] >= amount:
             store[chemical] -= amount
@@ -36,7 +16,7 @@ def get_reactions(chemical, amount, cookbook, store = {}):
         else:
             amount -= store[chemical]
             store[chemical] = 0
-    
+
     if chemical not in cookbook:
         return {chemical: amount}
 
@@ -44,7 +24,8 @@ def get_reactions(chemical, amount, cookbook, store = {}):
     recipe = cookbook[chemical]
     multiplier = ceil(amount/recipe.output)
     for component in recipe.components:
-        component_reqs = get_reactions(component.chemical, component.count * multiplier, cookbook, store)
+        component_reqs = get_reactions(
+            component.chemical, component.count * multiplier, cookbook, store)
         for c in component_reqs:
             if c in needed:
                 needed[c] += component_reqs[c]
@@ -65,10 +46,11 @@ def create_cookbook(input: str):
         input, output = line.split('=>')
         components = [i for i in input.split(',')]
         components = [c.strip().split(' ') for c in components]
-        components = [Component(int(c[0]),c[1]) for c in components]
+        components = [Component(int(c[0]), c[1]) for c in components]
         amount, chemical = output.strip().split(' ')
         result[chemical] = Chemical(int(amount), components)
     return result
+
 
 def get_input():
     file_path = os.path.join(os.path.dirname(__file__), 'day14.txt')
@@ -76,14 +58,34 @@ def get_input():
         contents = f.read()
         return contents
 
+
 def part1(input: str):
     cookbook = create_cookbook(input)
     needed = get_reactions('FUEL', 1, cookbook)
     return needed['ORE']
 
+
+def part2(input: str):
+    cookbook = create_cookbook(input)
+    ore = 1000000000000
+    store = {}
+    fuels = 0
+    while True:
+        needed = get_reactions('FUEL', 1, cookbook, store)
+        ore -= needed['ORE']
+        if fuels % 10000 == 0:
+            print(ore)
+        if ore < 0:
+            break
+        fuels += 1
+    return fuels
+
+
 def main():
     input = get_input()
     print(part1(input))
+    print(part2(input))
+
 
 if __name__ == "__main__":
     main()
